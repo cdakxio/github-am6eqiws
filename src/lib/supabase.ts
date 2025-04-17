7abe0655c94da159127db1c563cb11ac10a6fe4a
cdakxio <128591734+cdakxio@users.noreply.github.com>
2025-04-17 20:17:54 +0200

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://supabase.ia-temis.be/';
-const supabaseKey = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdXBhYmFzZSIsImlhdCI6MTc0NDExOTA2MCwiZXhwIjo0ODk5NzkyNjYwLCJyb2xlIjoic2VydmljZV9yb2xlIn0.mn0iGK1pOViIHu9Dlh2D24nuXKmY3NZxM1b0zka-0NU';
+
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
